d1d29cbd206cfc2a2fa5af774e926c77f814f2a3
Chen Pan <happychinapc@gmail.com>
2021-11-23 15:55:43 +0800

    Initial commit

---
+++ app/src/main/java/me/bytebeats/compose/bitcoin/BitcoinApplication.kt
@@ -0,0 +1,13 @@
+package me.bytebeats.compose.bitcoin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by bytebeats on 2021/11/23 : 15:50
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+
+@HiltAndroidApp
+class BitcoinApplication : Application()
---
+++ app/src/main/java/me/bytebeats/compose/bitcoin/ktx/Primitives.kt
@@ -0,0 +1,8 @@
+package me.bytebeats.compose.bitcoin.ktx
+
+/**
+ * Created by bytebeats on 2021/11/23 : 15:52
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+
